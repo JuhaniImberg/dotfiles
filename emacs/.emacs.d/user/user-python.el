f1596ee6e27937fff77ffe9fd81398167fc4b3dc
Juhani Imberg <juhani@imberg.fi>
2016-03-18 17:00:15 +0200

    emacs: use jedi instead of anaconda

--- emacs/.emacs.d/user/user-python.el
+++ emacs/.emacs.d/user/user-python.el
@@ -1,7 +1,13 @@
-(el-get-bundle company-anaconda
-  (progn
-    (add-to-list 'company-backends 'company-anaconda)
-    (add-hook 'python-mode-hook 'eldoc-mode)
-    (add-hook 'python-mode-hook 'anaconda-mode)))
+(el-get-bundle tkf/emacs-jedi
+  :load ("jedi-core.el")
+  :name jedi-core
+  :depends (epc python-environment))
+
+(el-get-bundle company-jedi
+  :depends (company-mode)
+  (add-hook 'python-mode-hook
+            (lambda () (add-to-list 'company-backends 'company-jedi))))
+
+(setq python-environment-directory "~/.virtualenvs/")
 
 (provide 'user-python)
